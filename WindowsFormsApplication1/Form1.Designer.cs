4fe74782514d4e16f5948aaf01cab497f69a7c1c
Tatyana <tan-l@mail.ru>
2020-12-19 15:51:15 +0800

    The 1st commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label13 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(67, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(264, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Приложение для интеграции с пакетами MS Office";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(165, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Вариант №8";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(23, 62);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Word";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 75);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(396, 13);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Открытие существующего документа. Добавление произвольной картинки.";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 88);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 4;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(23, 250);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(33, 13);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "Excel";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 263);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(231, 13);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "Объединение содержимого 2-х документов.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Выполнить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 117);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(382, 72);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(67, 101);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(265, 13);
+            this.label13.TabIndex = 10;
+            this.label13.Text = "↓Нажми, чтобы вставить скопированную картинку";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(409, 343);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Name = "Form1";
+            this.Text = "Лабораторная №5";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+//Событие нажатия на область для рисунка
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = Clipboard.GetImage();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+//Событие нажатия на первую кнопку "Выполнить"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Word.Application winWord = new Word.Application();
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog
+                {
+                    Multiselect = false,
+                    DefaultExt = "*.doc;*.docx",
+                    Filter = "Microsoft Word (*.doc*)|*.doc*",
+                    Title = "Выберите документ Word"
+                };
+                if (ofd.ShowDialog() != DialogResult.OK)
+                {
+                    MessageBox.Show("Вы не выбрали файл для открытия", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                string xlFileName = ofd.FileName;
+
+                Word.Document document = winWord.Documents.Open(xlFileName);
+
+                Clipboard.SetImage(pictureBox1.Image);
+                winWord.ActiveDocument.Paragraphs[1].Range.Paste();
+                winWord.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                winWord.Quit();
+            }
+        }
+//Событие нажатия на вторую кнопку "Выполнить"
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Excel.Application winExcel = new Excel.Application();
+            try
+            {
+                OpenFileDialog ofd1 = new OpenFileDialog
+                {
+                    Multiselect = false,
+                    DefaultExt = "*.xls;*.xlsx",
+                    Filter = "Microsoft Excel (*.xls*)|*.xls*",
+                    Title = "Выберите документ Excel №1"
+                };
+                if (ofd1.ShowDialog() != DialogResult.OK)
+                {
+                    MessageBox.Show("Вы не выбрали файл для открытия", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                string xlFile1Name = ofd1.FileName;
+
+                OpenFileDialog ofd2 = new OpenFileDialog
+                {
+                    Multiselect = false,
+                    DefaultExt = "*.xls;*.xlsx",
+                    Filter = "Microsoft Excel (*.xls*)|*.xls*",
+                    Title = "Выберите документ Excel №2"
+                };
+                if (ofd2.ShowDialog() != DialogResult.OK)
+                {
+                    MessageBox.Show("Вы не выбрали файл для открытия", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                string xlFile2Name = ofd2.FileName;
+
+                Excel.Workbook workbook1;
+                Excel.Workbook workbook2;
+                Excel.Workbook workbookFinal;
+                Excel.Worksheet worksheet1;
+                Excel.Worksheet worksheet2;
+                Excel.Worksheet worksheetFinal;
+
+                workbook1 = winExcel.Workbooks.Open(xlFile1Name);
+                worksheet1 = (Excel.Worksheet)workbook1.Sheets[1];
+                workbook2 = winExcel.Workbooks.Open(xlFile2Name);
+                worksheet2 = (Excel.Worksheet)workbook2.Sheets[1];
+                workbookFinal = winExcel.Workbooks.Add();
+                worksheetFinal = (Excel.Worksheet)workbookFinal.Sheets[1];
+
+                int iLastRow = worksheet1.Cells[worksheet1.Rows.Count, "A"].End[Excel.XlDirection.xlUp].Row;
+                var arrData = (object[,])worksheet1.Range["A1:Z" + iLastRow].Value;
+                int upBound = arrData.GetUpperBound(0) + 1;
+                worksheetFinal.Range["A1"].get_Resize(arrData.GetUpperBound(0), arrData.GetUpperBound(1)).Value = arrData;
+
+                iLastRow = worksheet2.Cells[worksheet1.Rows.Count, "A"].End[Excel.XlDirection.xlUp].Row;
+                arrData = (object[,])worksheet2.Range["A1:Z" + iLastRow].Value;
+                worksheetFinal.Range["A" + upBound].get_Resize(arrData.GetUpperBound(0), arrData.GetUpperBound(1)).Value = arrData;
+                SaveFileDialog sfd = new SaveFileDialog()
+                {
+                    //DefaultExt = "*.xls;*.xlsx",
+                    Filter = "Microsoft Excel (*.xls*)|*.xls*",
+                    Title = "Выберите место для сохранения"
+                };
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    workbookFinal.SaveAs(sfd.FileName);
+                }
+                winExcel.Quit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                winExcel.Quit();
+            }
+        }
+    }
+}
